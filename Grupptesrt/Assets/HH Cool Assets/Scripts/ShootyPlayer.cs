11dc8981a2addecaee3a15edecf17e4650682390
Snabbsly <herberthaegg@outlook.com>
2025-01-08 10:36:27 +0100

    My Stuff

---
+++ Grupptesrt/Assets/HH Cool Assets/Scripts/ShootyPlayer.cs
@@ -0,0 +1,21 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootyPlayer : MonoBehaviour
+{
+    [SerializeField] List<GameObject> projectiles;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("KeyCode Q"))
+        {
+            Instantiate(projectiles[0], transform.position, Quaternion.identity);  
+        }
+    }
+}
