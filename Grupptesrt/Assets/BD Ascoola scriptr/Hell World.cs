6d269d651dbcc2634d6d323744199b5076a3a9de
Benjamin Domingo <tandsonen@gmail.com>
2025-01-08 10:14:57 +0100

    Hell world :D

---
+++ Grupptesrt/Assets/BD Ascoola scriptr/Hell World.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class HellWorld : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.LogWarning("HELL WORLD :D");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
