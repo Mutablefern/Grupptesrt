d8a341411fede565f42d387aea1db15c780d7bc8
Benjamin Domingo <benjamin.domingo@learnet.se>
2025-01-10 10:43:48 +0100

    FIREBALL

--- Grupptesrt/Assets/BD Ascoola scriptr/Hell World.cs
+++ Grupptesrt/Assets/BD Ascoola scriptr/Hell World.cs
@@ -1,16 +1,20 @@
+using System.Collections;
 using UnityEngine;
 
 public class HellWorld : MonoBehaviour
 {
+    [SerializeField] GameObject Fire;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Debug.LogWarning("HELL WORLD :D");
+        StartCoroutine (HellWorlds());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator HellWorlds()
     {
-        
+        Debug.LogWarning("HELL WORLD :D");
+        Instantiate(Fire, transform.position, Random.rotation);
+        yield return new WaitForSeconds(Random.Range(1, 5));
+        StartCoroutine (HellWorlds());
     }
 }
